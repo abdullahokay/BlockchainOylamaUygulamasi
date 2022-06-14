9f2a147efc7b0cd32a8d1d4e657cc6c192e75876
abdullahokay <107299543+abdullahokay@users.noreply.github.com>
2022-06-14 22:12:42 +0300

    deprecated methods replaced

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
   const loadweb3 = async () => {
     if(window.ethereum) {
       window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable();
+      await window.ethereum.request({ method: "eth_requestAccounts" });
     } else if(window.web3) {
       window.ethereum = new Web3(window.web3.currentProvider);
     } else {
